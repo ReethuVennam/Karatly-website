bb35b1ee69f449d79f6fb2f64836170910ce731b
Dudimetla Sai <saiy63001@gmail.com>
2026-05-20 16:22:49 +0530

    user give access to select primary account

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -4,6 +4,7 @@ import FAQ from "../components/FAQ";
 export default function FAQPage() {
   return (
     <div className="bg-black text-white min-h-screen">
+      
       <Navbar />
       <div className="pt-28 px-6">
         <FAQ />
